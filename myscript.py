8e79afefb686c4a6556d2a119beaa2640bbfc329
Scapist <mohammedariful1998@gmail.com>
2023-03-30 02:01:06 -0400

    automated bisect

--- myscript.py
+++ myscript.py
@@ -1,4 +1,6 @@
 import os
-os.system('git bisect start HEAD HEAD~2')
+os.system('git bisect start')
+os.system('git bisect bad')
+os.system('git bisect start HEAD~2')
 os.system('git bisect run python manage.py test')
 os.system('git bisect reset')
